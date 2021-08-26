2c285267473452f501c3c263b53b1f78aaf43dba
B00ttichelli <donttrytofish@protonmail.com>
2021-08-26 10:26:32 +0300

    task

---
+++ src/CodewarsTasks/BasicProgram/function_3_multiplying_two_numbers.java
@@ -0,0 +1,14 @@
+package CodewarsTasks.BasicProgram;
+
+public class function_3_multiplying_two_numbers {
+
+    public static void main(String[] args) {
+        System.out.println(Kata.multiply(2,2)); // 2*2 = 4 :)
+    }
+
+    public static class Kata {
+        public static int multiply(int num1, int num2) {
+            return num1*num2;
+        }
+    }
+}
