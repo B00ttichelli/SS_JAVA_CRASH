b5c7092b1a1c43b20c157f13ef7803ce2d5cabf9
B00ttichelli <donttrytofish@protonmail.com>
2021-08-26 10:23:54 +0300

    Initial commit

---
+++ src/CodewarsTasks/BasicProgram/you_cant_code_under_pressure_number_1.java
@@ -0,0 +1,15 @@
+package CodewarsTasks.BasicProgram;
+
+public class you_cant_code_under_pressure_number_1 {
+
+    public static void main(String[] args) {
+
+        System.out.println(Java.doubleInteger(2)); //2*2=4
+    }
+    static class Java {
+        public static int doubleInteger(int i) {
+            // Double the integer and return it!
+            return i*2;
+        }
+    }
+}
