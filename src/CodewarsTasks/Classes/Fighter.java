68acf86edef445a5d73df4428866be8f5142875e
B00ttichelli <donttrytofish@protonmail.com>
2021-08-28 18:13:33 +0300

    task

---
+++ src/CodewarsTasks/Classes/Fighter.java
@@ -0,0 +1,16 @@
+package CodewarsTasks.Classes;
+
+public class Fighter {
+
+
+        public String name;
+        public int health, damagePerAttack;
+        public boolean isAttacker = false;
+        public Fighter(String name, int health, int damagePerAttack) {
+            this.name = name;
+            this.health = health;
+            this.damagePerAttack = damagePerAttack;
+        }
+    }
+
+
---
+++ src/CodewarsTasks/Classes/two_fighters_one_winner.java
@@ -0,0 +1,54 @@
+package CodewarsTasks.Classes;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class two_fighters_one_winner {
+
+    public static void main(String[] args) {
+
+        assertEquals("Lew", two_fighters_one_winner.declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew"));
+        assertEquals("Harry", two_fighters_one_winner.declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Harry"));
+        assertEquals("Harald", two_fighters_one_winner.declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harry"));
+        assertEquals("Harald", two_fighters_one_winner.declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harald"));
+        assertEquals("Harald", two_fighters_one_winner.declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Jerry"));
+        assertEquals("Harald", two_fighters_one_winner.declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Harald"));
+
+    }
+
+    public static String declareWinner(Fighter fighter1, Fighter fighter2, String firstAttacker) {
+        // Your code goes here. Have fun!
+        //Бойцов выдали не ООПшных никакой инкапсуляции
+
+        //определяем самого борзого
+        if (fighter1.name.equals(firstAttacker)) {
+            fighter1.isAttacker = true;
+        } else {
+            fighter2.isAttacker = true;
+        }
+
+        // начинаем раздавать друг другу люлей
+        while (true) {
+            if (fighter1.isAttacker) {
+                fighter2.health = fighter2.health - fighter1.damagePerAttack; // ай-ой больна
+                fighter1.isAttacker = false;
+                fighter2.isAttacker = true; // ну я щас тебе вмажу
+                if (fighter2.health <=0){
+                    break;
+                }
+            } else {
+
+                fighter1.health = fighter1.health - fighter2.damagePerAttack;
+                fighter2.isAttacker = false;
+                fighter1.isAttacker = true;
+                if(fighter1.health <=0){
+                    break;
+                }
+            }
+        }
+
+        return fighter1.health > 0 ? fighter1.name : fighter2.name; // смотрим кто труп
+    }
+
+
+}
