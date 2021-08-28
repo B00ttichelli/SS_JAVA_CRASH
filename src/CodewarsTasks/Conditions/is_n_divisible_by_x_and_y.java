089d60c537386db836c46140a2e98f727d9bed58
B00ttichelli <donttrytofish@protonmail.com>
2021-08-29 01:13:46 +0300

    task

--- src/CodewarsTasks/Conditions/is_n_divisible_by_x_and_y.java
+++ src/CodewarsTasks/Conditions/is_n_divisible_by_x_and_y.java
@@ -1,6 +1,31 @@
 package CodewarsTasks.Conditions;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class is_n_divisible_by_x_and_y {
+
+
+
+
+
+
+        @Test
+        public void test1() {
+            assertEquals(true, isDivisible(12,4,3));
+        }
+        @Test
+        public void test2() {
+            assertEquals(false, isDivisible(3,3,4));
+        }
+
+
+
+    public static boolean isDivisible(long n, long x, long y) {
+        return n%x==0&&n%y==0;
+    }
+
 
 
 }
