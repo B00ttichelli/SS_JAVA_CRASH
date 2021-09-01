eba1bf604ffb9399fdf5870aa86b9ecb0ad319e7
B00ttichelli <donttrytofish@protonmail.com>
2021-09-02 01:47:46 +0300

    task

---
+++ src/Loops/beginner_lost_without_a_map.java
@@ -0,0 +1,24 @@
+package Loops;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class beginner_lost_without_a_map {
+
+    @Test
+    public void sampleTests() {
+        assertArrayEquals(new int[] {2, 4, 6}, map(new int[] {1, 2, 3}));
+        assertArrayEquals(new int[] {8, 2, 2, 2, 8}, map(new int[] {4, 1, 1, 1, 4}));
+        assertArrayEquals(new int[] {2, 2, 2, 2, 2, 2}, map(new int[] {1, 1, 1, 1, 1, 1}));
+    }
+
+
+    public static int[] map(int[] arr) {
+
+
+            return Arrays.stream(arr).map(a->a*2).toArray();
+    }
+}
