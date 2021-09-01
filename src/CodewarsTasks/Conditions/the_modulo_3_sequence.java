9c3735f54cc0ca5724f095698568f95f8c31a102
B00ttichelli <donttrytofish@protonmail.com>
2021-09-02 00:33:59 +0300

    task

---
+++ src/CodewarsTasks/Conditions/the_modulo_3_sequence.java
@@ -0,0 +1,40 @@
+package CodewarsTasks.Conditions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class the_modulo_3_sequence
+
+
+{
+
+    @Test
+    public void testSomething() {
+        assertEquals(2, sequence(20));
+        assertEquals(2, sequence(148));
+        assertEquals(2, sequence(111));
+        assertEquals(2, sequence(118));
+        assertEquals(0, sequence(41));
+        assertEquals(0, sequence(85));
+        assertEquals(0, sequence(117));
+        assertEquals(1, sequence(82));
+        assertEquals(1, sequence(72));
+        assertEquals(1,sequence(5101394));
+        assertEquals(1, sequence(163));
+        assertEquals(1,sequence(90));
+    }
+
+    public static int sequence(int n) {
+        return 0;
+    }
+
+    /*Consider a sequence where the first two numbers are 0 and 1 and the next number of the sequence is the sum of the previous 2 modulo 3.
+ Write a function that finds the nth number of the sequence.*/
+
+
+//Не совсем понятные условия
+
+    //TODO:Решить позже
+
+}
