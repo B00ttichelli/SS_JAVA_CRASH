d583671693bf653623ba8931b67c4060ee1db186
B00ttichelli <donttrytofish@protonmail.com>
2021-09-02 20:15:46 +0300

    task

---
+++ src/CodewarsTasks/oop/l3375p34k.java
@@ -0,0 +1,46 @@
+package CodewarsTasks.oop;
+
+/*
+        a -> 4
+        e -> 3
+        l -> 1
+        m -> /^^\
+        o -> 0
+        u -> (_)
+*/
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class l3375p34k extends Encoder {
+
+        //where is a fucking OOP there ?
+    public static void main(String[] args) {
+         l3375p34k myEncoder = new l3375p34k();
+
+
+
+            assertTrue("empty string", myEncoder.encode("").equals(""));
+            assertTrue("abcdef string", myEncoder.encode("abcdef").equals("4bcd3f"));
+
+    }
+
+    public String encode(String source){
+
+        return source == null?"":source.replace("a","4").replace("e","3").
+                replace("l","1").
+                replace("m","/^^\\").replace("o","0").
+                replace("u","(_)").replace("A","4").replace("E","3").
+                replace("L","1").
+                replace("M","/^^\\").replace("O","0").replace("U","(_)");
+    }
+}
+
+abstract class Encoder{
+    public abstract String encode(String source);
+}
+
+
+
---
+++ src/CodewarsTasks/oop/rotations_and_reflections_i.java
@@ -0,0 +1,8 @@
+package CodewarsTasks.oop;
+
+public class rotations_and_reflections_i {
+
+
+    //
+    //не совсем понятные условия задачи
+}
