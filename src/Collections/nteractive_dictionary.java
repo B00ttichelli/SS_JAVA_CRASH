bd512d71ca9e6690be9ef1dd83c9cba3df4458ff
B00ttichelli <donttrytofish@protonmail.com>
2021-09-07 02:36:02 +0300

    task

---
+++ src/Collections/nteractive_dictionary.java
@@ -0,0 +1,51 @@
+package Collections;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class nteractive_dictionary {
+
+    @Test
+    public void Tests()
+    {
+        Dictionary d = new Dictionary();
+
+        d.newEntry("Apple", "A fruit");
+        assertEquals("A fruit", d.look("Apple"));
+
+        d.newEntry("Soccer", "A sport");
+        assertEquals("A sport", d.look("Soccer"));
+
+        assertEquals("Cant find entry for Hi", d.look("Hi"));
+        assertEquals("Cant find entry for Ball", d.look("Ball"));
+
+        d.newEntry("soccer", "a sport");
+        assertEquals("a sport", d.look("soccer"));
+    }
+
+
+     class Dictionary {
+        private Map<String,String> dict = new HashMap<>();
+
+        public Dictionary(){
+            // your code
+        }
+
+        public void newEntry(String key, String value){
+            // your code
+
+            dict.put(key,value);
+
+
+        }
+
+        public String look(String key){
+            // your code
+            return dict.get(key)==null?"Cant find entry for "+key:dict.get(key);
+        }
+    }
+}
