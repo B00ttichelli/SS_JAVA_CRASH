2de789517dcd2f890adaa0e5cf5a6b9e71293483
B00ttichelli <donttrytofish@protonmail.com>
2021-09-07 02:00:25 +0300

    task

---
+++ src/Collections/push_a_hash_slash_an_object_into_array.java
@@ -0,0 +1,28 @@
+package Collections;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class push_a_hash_slash_an_object_into_array {
+
+
+    @Test
+    public void testSomething() {
+        List<String> items = PushAnObjectIntoArray.push();
+        assertEquals(1,items.size());
+        assertEquals("an object", items.get(0));
+    }
+
+     static class PushAnObjectIntoArray {
+        public static List<String> push(){
+            List<String> items = new ArrayList<>();
+            items.add ("an object");
+            return items;
+        }
+    }
+
+}
