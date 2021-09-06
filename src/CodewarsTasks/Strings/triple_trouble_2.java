8070b762b7cfa24529d23f92a1186e385cf5903a
B00ttichelli <donttrytofish@protonmail.com>
2021-09-06 14:24:00 +0300

    task

---
+++ src/CodewarsTasks/Strings/triple_trouble_2.java
@@ -0,0 +1,23 @@
+package CodewarsTasks.Strings;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class triple_trouble_2 {
+
+    @Test
+    public void tests() {
+        assertEquals("ttlheoiscstk", Kata.tripleTrouble("this", "test", "lock"));
+        assertEquals("abcabc", Kata.tripleTrouble("aa","bb","cc"));
+        assertEquals("Batman", Kata.tripleTrouble("Bm", "aa", "tn"));
+        assertEquals("LexLuthor", Kata.tripleTrouble("LLh","euo","xtr"));
+    }
+
+    static class Kata {
+        public static String tripleTrouble(String one, String two, String three) {
+            // Solution
+        }
+    }
+
+}
