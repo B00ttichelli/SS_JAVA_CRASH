4387ecf0b828757799404192f227c65dcd9ec3d4
B00ttichelli <donttrytofish@protonmail.com>
2021-09-06 14:35:21 +0300

    task

--- src/CodewarsTasks/Strings/triple_trouble_2.java
+++ src/CodewarsTasks/Strings/triple_trouble_2.java
@@ -9,14 +9,19 @@ public class triple_trouble_2 {
     @Test
     public void tests() {
         assertEquals("ttlheoiscstk", Kata.tripleTrouble("this", "test", "lock"));
-        assertEquals("abcabc", Kata.tripleTrouble("aa","bb","cc"));
+        assertEquals("abcabc", Kata.tripleTrouble("aa", "bb", "cc"));
         assertEquals("Batman", Kata.tripleTrouble("Bm", "aa", "tn"));
-        assertEquals("LexLuthor", Kata.tripleTrouble("LLh","euo","xtr"));
+        assertEquals("LexLuthor", Kata.tripleTrouble("LLh", "euo", "xtr"));
     }
 
     static class Kata {
         public static String tripleTrouble(String one, String two, String three) {
             // Solution
+            String result = "";
+            for (int i = 0; i < one.length(); i++) {
+                result = result + one.charAt(i) + two.charAt(i) + three.charAt(i);
+            }
+            return result;
         }
     }
 
