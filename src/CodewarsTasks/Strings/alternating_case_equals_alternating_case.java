fd4d5a0c5acb92b486af9a4125f5e8afb97a2800
B00ttichelli <donttrytofish@protonmail.com>
2021-09-07 01:45:48 +0300

    task

---
+++ src/CodewarsTasks/Strings/alternating_case_equals_alternating_case.java
@@ -0,0 +1,50 @@
+package CodewarsTasks.Strings;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class alternating_case_equals_alternating_case {
+    @Test
+    public void fixedTests() {
+        assertEquals("HELLO WORLD", StringUtils.toAlternativeString("hello world"));
+        assertEquals("hello world", StringUtils.toAlternativeString("HELLO WORLD"));
+        assertEquals("HELLO world", StringUtils.toAlternativeString("hello WORLD"));
+        assertEquals("hEllO wOrld", StringUtils.toAlternativeString("HeLLo WoRLD"));
+        assertEquals("Hello World", StringUtils.toAlternativeString(StringUtils.toAlternativeString("Hello World")));
+        assertEquals("12345", StringUtils.toAlternativeString("12345"));
+        assertEquals("1A2B3C4D5E", StringUtils.toAlternativeString("1a2b3c4d5e"));
+        assertEquals("sTRINGuTILS.TOaLTERNATINGcASE", StringUtils.toAlternativeString("StringUtils.toAlternatingCase"));
+    }
+
+    @Test
+    public void kataTitleTests() {
+        assertEquals("ALTerNAtiNG CaSe", StringUtils.toAlternativeString("altERnaTIng cAsE"));
+        assertEquals("altERnaTIng cAsE", StringUtils.toAlternativeString("ALTerNAtiNG CaSe"));
+        assertEquals("ALTerNAtiNG CaSe <=> altERnaTIng cAsE", StringUtils.toAlternativeString("altERnaTIng cAsE <=> ALTerNAtiNG CaSe"));
+        assertEquals("altERnaTIng cAsE <=> ALTerNAtiNG CaSe", StringUtils.toAlternativeString("ALTerNAtiNG CaSe <=> altERnaTIng cAsE"));
+    }
+
+    static class StringUtils {
+
+        public static String toAlternativeString(String string) {
+            // your code here!
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (char c:string.toCharArray()
+                 ) {
+                if (Character.isLowerCase(c)) {
+                    stringBuilder.append(Character.toUpperCase(c));
+                } else {
+                    stringBuilder.append(Character.toLowerCase(c));
+                }
+            }
+            return stringBuilder.toString();
+        }
+    }
+
+}
